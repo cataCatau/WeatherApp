462cc814a34881b71153b0968252cf78bd1ee5ff
cataCatau <tarcacatalin13@gmail.com>
2025-12-10 23:01:54 +0200

    extragere coordonate folosind API

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-use eframe::egui;
+use eframe::{egui, wgpu::hal::auxil::db};
 use egui::Align;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct GeoSearch {
+    results: Option<Vec<GeoLocation>>,
+}
+#[derive(Deserialize, Debug)]
+struct GeoLocation {
+    name: String,
+    latitude: f64,
+    longitude: f64,
+    country: Option<String>,
+}
 struct App {
     city_name: String,
 }
@@ -15,23 +28,45 @@ impl Default for App {
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading(egui::RichText::new("Weather Dashboard").size(24.0).strong());
             ui.with_layout(egui::Layout::top_down(Align::Center), |ui| {
-                ui.label("Introduceti orasul");
+                ui.heading(egui::RichText::new("Weather Dashboard").size(24.0).strong());
+                ui.add_space(250.0);
+                ui.label(egui::RichText::new("Introduceti orasul").size(24.0));
                 ui.text_edit_singleline(&mut self.city_name);
 
                 if ui.button("search").clicked() {
-                    println!("in {} nu stim ce vreme e", self.city_name);
+                    let city=self.city_name.clone();
+                    tokio::spawn(async move {
+                        let geo_url = format!(
+                            "https://geocoding-api.open-meteo.com/v1/search?name={}&count=1&language=ro&format=json",
+                            city);
+                        match reqwest::get(&geo_url).await {
+                            Ok(resp) => {
+                                match resp.json::<GeoSearch>().await {
+                                    Ok(geo_data) => {
+                                        if let Some(results) = geo_data.results {
+                                            if let Some(loc) = results.first() {
+                                                println!("{},{}", loc.latitude, loc.longitude);
+                                            }
+                                        }
+                                    }
+                                    Err(_) => {}
+                                }
+                            }
+                            Err(_) => {}
+                        }
+                    });
                 }
             });
         });
     }
 }
-fn main() -> eframe::Result<()> {
+#[tokio::main]
+async fn main() -> eframe::Result<()> {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([1920.0, 1080.0])
-            .with_title("Egui Example"),
+            .with_title("WeatherDashboard"),
         ..Default::default()
     };
 
